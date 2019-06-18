e2717e99f630759dc3c00e73edaccff373b95a52
Blake Sanie <blake@sanie.com>
2019-06-17 17:38:08 -0700

    sign up functionality + UI made, can switch between pages. Pretty sweet

---
+++ signup/signup.js
@@ -0,0 +1,41 @@
+$("#create").click(function() {
+  var first = $("#first").val();
+  var last = $("#last").val();
+  var email = $("#email").val();
+  var password1 = $("#password1").val();
+  var password2 = $("#password2").val();
+  if (
+    first.length *
+      last.length *
+      email.length *
+      password1.length *
+      password2.length ==
+    0
+  ) {
+    alert("please complete all fields");
+    return;
+  }
+  if (password1 != password2) {
+    alert("passwords do not match");
+    return;
+  }
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password1)
+    .then(function() {
+      console.log("user made!");
+      var user = firebase.auth().currentUser;
+      user.updateProfile({
+        displayName: first + " " + last
+      });
+      firebase
+        .firestore()
+        .collection("users")
+        .add({
+          id: user.uid
+        });
+    })
+    .catch(function(error) {
+      alert(error.message);
+    });
+});
