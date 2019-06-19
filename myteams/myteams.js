6d961adb7e03c9da0638b9637adbc312ef56531b
Blake Sanie <blake@sanie.com>
2019-06-18 17:28:40 -0700

    creating teams writes to db!

--- myteams/myteams.js
+++ myteams/myteams.js
@@ -1,7 +1,34 @@
+if (!firebase.auth().currentUser) {
+  console.log(firebase.auth().currentUser);
+  window.location.replace("../login");
+}
+
 $(".cell").on("click", function() {
   if ($(this).is("#new")) {
     console.log("new team");
+    $("#modal").addClass("onTop");
   } else {
     console.log("team pressed");
   }
+});
+
+$("#cancel").click(function() {
+  $("#modal").removeClass("onTop");
+});
+
+$("#create").click(function() {
+  var name = $("#nameInput").val();
+  var imageUrl = $("#imageInput").val();
+  if (name.length * imageUrl.length == 0) {
+    alert("please fill all fields");
+    return;
+  }
+  firebase
+    .firestore()
+    .collection("teams")
+    .doc(name)
+    .set({
+      imageUrl: imageUrl,
+      coaches: [firebase.auth().currentUser.uid]
+    });
 });
