af5d7b8364dc9d26712e2986f581215e39780eae
Blake Sanie <blake@sanie.com>
2019-06-19 09:45:50 -0700

    created teams are seen in ui

--- myteams/myteams.js
+++ myteams/myteams.js
@@ -1,7 +1,54 @@
-if (!firebase.auth().currentUser) {
-  console.log(firebase.auth().currentUser);
-  window.location.replace("../login");
-}
+firebase.auth().onAuthStateChanged(function(user) {
+  console.log("user change");
+  if (!user) {
+    window.location.replace("../login");
+  }
+  var role;
+  firebase
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .get()
+    .then(function(doc) {
+      role = doc.data().role;
+    })
+    .then(function() {
+      if (role != "coach") {
+        window.location.replace("../login");
+      }
+      console.log("got here 1");
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(user.uid)
+        .get()
+        .then(function(doc) {
+          var teams = doc.data().teams;
+          for (var team of teams) {
+            console.log(team);
+            firebase
+              .firestore()
+              .collection("teams")
+              .doc(team)
+              .get()
+              .then(function(doc) {
+                console.log(doc);
+                //console.log(doc.data());
+                $("#new").after(
+                  "<div class='cell' ><div class='thumb' style='background-image:url(" +
+                    doc.data().imageUrl +
+                    ");' ></div><p>" +
+                    doc.id +
+                    "</p></div>"
+                );
+              });
+          }
+        });
+    })
+    .catch(function(error) {
+      alert(error.message);
+    });
+});
 
 function previewPhoto() {
   $("#photoPreview").css({
@@ -29,7 +76,7 @@ $("#create").click(function() {
     alert("please fill all fields");
     return;
   }
-  firebase
+  firebase //security rules. can write if doc does not exist or coach is listed in coaches
     .firestore()
     .collection("teams")
     .doc(name)
@@ -37,7 +84,21 @@ $("#create").click(function() {
       imageUrl: imageUrl,
       coaches: [firebase.auth().currentUser.uid]
     })
-    .then(function() {})
+    .then(function() {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid)
+        .update({
+          teams: firebase.firestore.FieldValue.arrayUnion(name) //safe way to add to array
+        })
+        .then(function() {
+          location.reload();
+        })
+        .catch(function(error) {
+          alert(error.message);
+        });
+    })
     .catch(function(error) {
       alert(error.message);
     });
