4a7bc42931f7dddfbe0b6a7dfcc30cb99b7b41e7
Blake Sanie <blake@sanie.com>
2019-07-19 12:07:01 -0700

    expand/collapsing of cells works, made to not always maintain full width

--- teams/video/video.js
+++ teams/video/video.js
@@ -24,6 +24,9 @@ $("body").on("click", ".expander", function() {
   // if this has class expanded, flip all to up
   if ($(this).hasClass("expanded")) {
     $(this).removeClass("expanded");
+    // collapse cells
+    var row = $(this).closest("tr");
+    collapseRow(row);
   } else {
     $(this)
       .parent()
@@ -31,8 +34,46 @@ $("body").on("click", ".expander", function() {
       .find(".expander")
       .removeClass("expanded");
     $(this).addClass("expanded");
+    //expand cells
+    var row = $(this).closest("tr");
+    expandRow(row);
+  }
+  if (
+    $(this)
+      .closest("tr")
+      .hasClass("round")
+  ) {
+    var sortBy = $(this)
+      .parent()
+      .text()
+      .toLowerCase();
+    console.log(sortBy);
   }
 });
+
+function collapseRow(row) {
+  var type = row.attr("class");
+  var startingIndex = row.index();
+  for (var i = startingIndex + 1; i < $("tr").length; i++) {
+    var currentRow = $("tr").eq(i);
+    if (currentRow.hasClass("event")) {
+      break;
+    }
+    currentRow.addClass("hidden");
+  }
+}
+
+function expandRow(row) {
+  var type = row.attr("class");
+  var startingIndex = row.index();
+  for (var i = startingIndex + 1; i < $("tr").length; i++) {
+    var currentRow = $("tr").eq(i);
+    if (currentRow.hasClass("event") || currentRow.hasClass("." + type)) {
+      break;
+    }
+    currentRow.removeClass("hidden");
+  }
+}
 
 $("#creatorLink").click(function() {
   window.location.href = "../creator/?name=" + encodeURI(teamName);
